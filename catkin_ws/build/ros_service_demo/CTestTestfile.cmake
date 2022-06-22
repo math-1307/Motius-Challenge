69f95cb3f93d791e1c6bb2c733446781f2160271
math-1307 <107972931+math-1307@users.noreply.github.com>
2022-06-22 10:53:41 +0200

    Deleted build directory
    Build files have to be built on the system and already built files cannot be used

--- catkin_ws/build/ros_service_demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vamika/ROS/Motius_Challenge/catkin_ws/src/ros_service_demo
-# Build directory: /home/vamika/ROS/Motius_Challenge/catkin_ws/build/ros_service_demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
